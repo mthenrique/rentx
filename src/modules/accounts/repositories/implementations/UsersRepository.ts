5edac389716815af0fcdc9532f86ade2279f4c29
Mateus <mthenriqueo@gmail.com>
2021-09-29 22:20:22 -0300

    implementado criptografia de senha e nao permitir email duplicado

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,7 +10,7 @@ class UsersRepository implements IUsersRepository {
     constructor(){
         this.repository = getRepository(User);
     }
-
+    
     async create({name, email, driver_license, password}: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
             name,
@@ -20,6 +20,11 @@ class UsersRepository implements IUsersRepository {
         });
 
         await this.repository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        const user = await this.repository.findOne({ email });
+        return user;
     }
 }
 
