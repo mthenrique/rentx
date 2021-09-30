d4e3d665d698667a24542557a70d663acba3503f
Mateus <mthenriqueo@gmail.com>
2021-09-29 21:22:44 -0300

    Implementado repositorio de usuarios

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,13 @@
 import { container } from "tsyringe";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 
 //ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -16,4 +19,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
     "SpecificationRepository",
     SpecificationRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );
