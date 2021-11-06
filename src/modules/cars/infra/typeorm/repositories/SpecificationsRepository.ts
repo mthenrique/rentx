abbc399a888df2500bf4e50162d9c68d34b7dabf
Mateus <mthenriqueo@gmail.com>
2021-11-06 12:46:13 -0300

    Refatoração da aplicação

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,9 +1,7 @@
+import { ICreateSpecificationDTO, ISpecificationRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
-import {
-    ISpecificationRepository,
-    ICreateSpecificationDTO,
-} from "../ISpecificationsRepository";
+import { Specification } from "../entities/Specification";
+
 
 class SpecificationRepository implements ISpecificationRepository {
     private repository: Repository<Specification>;
