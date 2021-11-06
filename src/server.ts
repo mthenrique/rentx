6c0e6eb00ac95065c2d4eb5aa71db0b3d394c213
Mateus <mthenriqueo@gmail.com>
2021-11-06 12:00:13 -0300

    Refatorado importações

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import swaggerUi from "swagger-ui-express";
 
 import "./database";
 
-import "./shared/container";
+import "@shared/container";
+import { AppError } from "@errors/AppError";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-import { AppError } from "./errors/AppError";
 
 // import "./database";
 
