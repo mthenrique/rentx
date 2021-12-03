253a996e79d82a353d42974e454bc6fb24b5af93
Mateus <mthenriqueo@gmail.com>
2021-12-03 20:35:45 -0300

    Inserindo template engine para envio de email

--- src/shared/container/providers/IMailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/IMailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { IMailProvider } from "../IMailProvider";
 import nodemailer, { Transporter } from "nodemailer";
-import { AwaitKeyword } from "typescript";
+import handlebars from "handlebars";
+import fs from "fs"
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -24,13 +25,18 @@ class EtherealMailProvider implements IMailProvider {
         }).catch(err => console.log(err));
     }
     
-    async sendMail(to: string, subject: string, body: string): Promise<void> {
+    async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+        const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+        const templateParse = handlebars.compile(templateFileContent);
+
+        const templateHTML = templateParse(variables);
+
         const message = await this.client.sendMail({
             to,
             from: "Rentx <noreplay@rentx.com.br>",
             subject,
-            text: body,
-            html: body,
+            html: templateHTML,
         });
 
         console.log("Message sent: %s", message.messageId);
